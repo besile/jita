13e8ab9d6a72514bcfcaa04f012b81b117e18290
xubinghua <xbh@bitauto.com>
2016-01-18 15:12:18 +0800

    initial commit

---
+++ Jita.Common.Attr/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jita.Common.Attr
+{
+    public class Class1
+    {
+    }
+}
---
+++ Jita.Data.Model/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jita.Data.Model
+{
+    public class Class1
+    {
+    }
+}
